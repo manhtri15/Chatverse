c997dc0f89af2b4c23ee5719bb27cc98cc1205eb
songlingm <91147319+songlingm@users.noreply.github.com>
2022-05-16 15:35:11 +0700

    Lam
    Sua trang login tam thoi

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import {Row, Col, Button, Typography} from 'antd';
 import firebase, {auth, db} from '../../firebase/config';
 import {addDocument, generateKeywords} from '../../firebase/services';
+const LaucherIcon = require('../../assets/ic_laucher.png');
 const {Title} = Typography;
 
-// const fbProvider = new firebase.auth.FacebookAuthProvider();
+const fbProvider = new firebase.auth.FacebookAuthProvider();
 const googleProvider = new firebase.auth.GoogleAuthProvider();
 
 export default function Login(){
@@ -12,7 +13,7 @@ export default function Login(){
 		const {additionalUserInfo, user} = await auth.signInWithPopup(provider);
 		let users = await db
 			.collection('users')
-			.where('email', '==',additionalUserInfo?.profile.email)
+			.where('email', '==', additionalUserInfo?.profile.email)
 			.limit(1)
 			.get();
 		console.log(users.docs.length);
@@ -29,26 +30,38 @@ export default function Login(){
 	};
 	
 	return (
-		<div>
-			<Row justify='center' style={{height: 800}}>
-				<Col span={8}>
-					<Title style={{textAlign: 'center'}} level={3}>
-						Fun Chat
-					</Title>
+		<div style={{backgroundColor: '#336b87', height: '100vh',display:"flex", justifyContent: "center",alignItems: 'center'}}>
+			
+			
+			<div style={{
+				width: 800,
+				height: 500,
+				padding: 64,display:"flex", justifyContent: "center",alignItems: 'center',
+				backgroundColor: '#90AFC5',
+				borderRadius: 8,
+				flexDirection:'column'
+			}}>
+				<img style={{marginLeft: 16, marginRight: 16, marginBottom: 32}} src={LaucherIcon} width='200' height='200'/>
+				
+				<Title style={{textAlign: 'center', color: '#9900ff'}} level={6}>ĐĂNG NHẬP</Title>
+				
+				<div style={{marginTop:60,flexDirection:'row',justifyContent:'space-between'}}>
 					<Button
-						style={{width: '100%', marginBottom: 5}}
+						style={{marginLeft: 16,width: 200,
+							height: 50,}}
 						onClick={() => handleLogin(googleProvider)}
 					>
 						Đăng nhập bằng Google
 					</Button>
-					{/*<Button*/}
-					{/*  style={{ width: '100%' }}*/}
-					{/*  onClick={() => handleLogin(fbProvider)}*/}
-					{/*>*/}
-					{/*  Đăng nhập bằng Facebook*/}
-					{/*</Button>*/}
-				</Col>
-			</Row>
+					<Button
+						style={{marginLeft: 16,width: 200,
+							height: 50,}}
+						onClick={() => handleLogin(fbProvider)}
+					>
+						Đăng nhập bằng Facebook
+					</Button>
+				</div>
+			</div>
 		</div>
 	);
 }
